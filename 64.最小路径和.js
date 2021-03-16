c57a1b045a3594d6c26464d87bc87e4f6a9cc84f
JasonWu008 <1179858091@qq.com>
2021-03-16 19:19:59 +0800

    feat: 64.最小路径和

---
+++ 64.最小路径和.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=javascript
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+ var minPathSum = function(grid) {
+  let m = grid.length, n = grid[0].length;
+  let dp = new Array(m);
+  for(let i = 0; i < m; i++) {
+    dp[i] = new Array(n);
+    if(i === 0) dp[i][0] = grid[0][0];
+    else dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+  for(let j = 1; j < n; j++) {
+    dp[0][j] = dp[0][j - 1] + grid[0][j];
+  }
+  for(let i = 1; i < m; i++) {
+    for(let j = 1; j < n; j++) {
+      dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
+// @lc code=end
+
